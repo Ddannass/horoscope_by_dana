9d08281650bec099af0620239c050cc39be82be2
Ddannass <danasera@yandex.ru>
2026-06-14 19:46:54 +0400

    Create sw.js

---
+++ assets/sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'astro-day-v2';
+const ASSETS = [
+  './index.html',
+  './assets/icon.svg',
+  './assets/manifest.webmanifest',
+  './assets/sw.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
